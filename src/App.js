5eed479ecfa6d82c658db1531469d93f3f8fc348
marmon179 <marmon179@gmail.com>
2021-12-15 11:10:49 +0300

    form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+import countries from "./countries"
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [country, setCountry] = useState("")
+    const [acceptedTerms, setAcceptedTerms] = useState(false)
+
+    const handleSubmit = (event) => {
+        alert(`
+      Адрес электронной почты: ${email}
+      Пароль: ${password}
+      Страна: ${country}
+      Согласия с условиями: ${acceptedTerms}
+    `)
+        event.preventDefault()
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>Создать аккаунт</h1>
+
+            <label>
+                Email:
+                <input
+                    name="email"
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    required/>
+            </label>
+
+            <label>
+                Пароль:
+                <input
+                    name="password"
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required/>
+            </label>
+
+            <label>
+                Страна:
+                <select
+                    name="country"
+                    value={country}
+                    onChange={e => setCountry(e.target.value)}
+                    required>
+                    <option key=""/>
+                    {countries.map(country => (
+                        <option key={country}>{country}</option>
+                    ))}
+                </select>
+            </label>
+
+            <label>
+                <input
+                    name="acceptedTerms"
+                    type="checkbox"
+                    onChange={e => setAcceptedTerms(e.target.value)}
+                    required/>
+                I Принять правила игры
+            </label>
+
+            <button>Отправить</button>
+        </form>
+    )
 }
-
-export default App;
---
+++ src/countries.js
@@ -0,0 +1,12 @@
+export default [
+    'Austria',
+    'Denmark',
+    'France',
+    'Germany',
+    'India',
+    'Italy',
+    'Poland',
+    'Russia',
+    'Sweden',
+    'United States'
+];
